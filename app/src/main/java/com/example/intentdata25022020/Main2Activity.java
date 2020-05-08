edca166590526c3a84e8b553e0a9b410115858bb
luongthanhdat0707 <luongthanhdat0707>
2020-05-08 15:59:05 +0700

    init

---
+++ app/src/main/java/com/example/intentdata25022020/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.example.intentdata25022020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+//        if(intent != null){
+//            String value = intent.getStringExtra("chuoi");
+//            Log.d("BBB", value);
+//        }
+        if (intent != null) {
+            String[] arrayStrings = intent.getStringArrayExtra("array");
+            Log.d("BBB", arrayStrings.length +"");
+        }
+    }
+}
